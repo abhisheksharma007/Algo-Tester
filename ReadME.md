958e448e3e37f153b1bccceafc2af0644c149d53
Abhishek Sharma <67024781+abhisheksharma007@users.noreply.github.com>
2024-09-11 03:52:24 +0530

    updated bug fixes, results are now being displayed (#2)

--- ReadME.md
+++ ReadME.md
@@ -14,3 +14,7 @@ source ./venv/bin/activate
 ```sh
 pip install -r requirements.txt
 ```
+
+```sh
+streamlit run app.py --server.headless true --server.runOnSave true
+```
--- backtest_runner.py
+++ backtest_runner.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from strategies import get_strategy
+import ipdb
+import traceback
 
 def run_backtest(data, initial_investment, stop_loss_percentage, strategy_name, strategy_params):
     """
@@ -12,53 +14,64 @@ def run_backtest(data, initial_investment, stop_loss_percentage, strategy_name, 
     :return: final_value, trades
     """
     # Get strategy function
-    strategy_func = get_strategy(strategy_name)
     
-    # Apply the selected strategy
-    signals = strategy_func(data, **strategy_params)
-
-    # Initialize portfolio variables
-    portfolio = pd.DataFrame(index=data.index)
-    portfolio['holdings'] = 0.0
-    portfolio['cash'] = initial_investment
-    portfolio['total'] = initial_investment
-
-    position = 0
-    entry_price = 0
-    trades = []
-    trailing_stop_price = 0
-
-    for date, signal in signals.iterrows():
-        if signal['positions'] == 1.0:  # Buy signal
-            position = portfolio['cash'] / data.loc[date, 'Close']
-            entry_price = data.loc[date, 'Close']
-            trailing_stop_price = entry_price  # Initialize trailing stop price
-            portfolio.at[date, 'holdings'] = position
-            portfolio.at[date, 'cash'] = 0.0
-            trades.append({'date': date, 'type': 'buy', 'price': entry_price})
+    try:
+        strategy_func = get_strategy(strategy_name)
         
-        elif signal['positions'] == -1.0:  # Sell signal
+        # Apply the selected strategy
+        signals = strategy_func(data, **strategy_params)
+
+        # Initialize portfolio variables
+        portfolio = pd.DataFrame(index=data.index)
+        portfolio['holdings'] = 0.0
+        portfolio['cash'] = initial_investment
+        portfolio['total'] = initial_investment
+
+        position = 0
+        entry_price = 0
+        trades = []
+        trailing_stop_price = 0
+
+        for date, signal in signals.iterrows():
+            if isinstance(date, pd.Series):
+                date = date.iloc[0]
+            date = date.strftime("%Y-%m-%d")
+            
+            spot_price = float(data.loc[date, 'Close'])
+
+            if signal['positions'] == 1.0:  # Buy signal
+                entry_price = spot_price
+                position = portfolio.at[date, 'cash'] / entry_price
+                trailing_stop_price = entry_price  # Initialize trailing stop price
+                portfolio.at[date, 'holdings'] = position
+                portfolio.at[date, 'cash'] = 0.0
+                trades.append({'date': date, 'type': 'buy', 'price': entry_price, 'holdings': portfolio.at[date, 'holdings'], 'cash': portfolio.at[date, 'cash']})
+            
+            elif signal['positions'] == -1.0:  # Sell signal
+                if position > 0:
+                    exit_price = spot_price
+                    profit_loss = (exit_price - entry_price) * position
+                    portfolio.at[date, 'cash'] = position * exit_price
+                    portfolio.at[date, 'holdings'] = 0.0
+                    portfolio.at[date, 'total'] = portfolio.at[date, 'cash'] + portfolio.at[date, 'holdings']
+                    trades.append({'date': date, 'type': 'sell', 'price': exit_price, 'profit_loss': profit_loss, 'holdings': portfolio.at[date, 'holdings'], 'cash': portfolio.at[date, 'cash']})
+                    position = 0
+
+            # Trailing stop loss logic
             if position > 0:
-                exit_price = data.loc[date, 'Close']
-                profit_loss = (exit_price - entry_price) * position
-                portfolio.at[date, 'cash'] = position * exit_price
-                portfolio.at[date, 'holdings'] = 0.0
-                portfolio.at[date, 'total'] = portfolio.at[date, 'cash'] + portfolio.at[date, 'holdings']
-                trades.append({'date': date, 'type': 'sell', 'price': exit_price, 'profit_loss': profit_loss})
-                position = 0
+                if spot_price > trailing_stop_price:
+                    trailing_stop_price = spot_price * (1 - stop_loss_percentage)
+                elif spot_price < trailing_stop_price:
+                    exit_price = spot_price
+                    profit_loss = (exit_price - entry_price) * position
+                    portfolio.at[date, 'cash'] = position * exit_price
+                    portfolio.at[date, 'holdings'] = 0.0
+                    portfolio.at[date, 'total'] = portfolio.at[date, 'cash'] + portfolio.at[date, 'holdings']
+                    trades.append({'date': date, 'type': 'sell', 'price': exit_price, 'profit_loss': profit_loss, 'holdings': portfolio.at[date, 'holdings'], 'cash': portfolio.at[date, 'cash']})
+                    position = 0
 
-        # Trailing stop loss logic
-        if position > 0:
-            if data.loc[date, 'Close'] > trailing_stop_price:
-                trailing_stop_price = data.loc[date, 'Close'] * (1 - stop_loss_percentage)
-            elif data.loc[date, 'Close'] < trailing_stop_price:
-                exit_price = data.loc[date, 'Close']
-                profit_loss = (exit_price - entry_price) * position
-                portfolio.at[date, 'cash'] = position * exit_price
-                portfolio.at[date, 'holdings'] = 0.0
-                portfolio.at[date, 'total'] = portfolio.at[date, 'cash'] + portfolio.at[date, 'holdings']
-                trades.append({'date': date, 'type': 'sell', 'price': exit_price, 'profit_loss': profit_loss})
-                position = 0
 
-    final_value = portfolio['total'].iloc[-1]
-    return final_value, trades
+        final_value = portfolio['total'].iloc[-1]
+        return final_value, trades
+    except:
+        print(traceback.format_exc())
